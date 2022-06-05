5bd6f21b90cbfda417d5e532d5c47eaccf56e27e
Burak <mburak.demir.059@gmail.com>
2022-06-05 14:36:43 +0300

    Updates

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,8 +10,34 @@ function Home() {
     <div className='home'>
       {
         loading ? <div>Loading...</div> :
-        data && data.slice(0,10).map((item)=>(
-          <div className="">{item.symbol}</div>
+        data && data.slice(0,25).map((item)=>(
+          <div className="coin-container">
+            <div className="coin-row">
+              <div className="coin">
+                <img src={item.image} alt="" />
+                <h1>{item.name}</h1>
+                <p className='coin-symbol'>{item.symbol}</p>
+              </div>
+              <div className="coin-data">
+                <p className="coin-price">${item.current_price}</p>
+
+                {
+                  item.price_change_percentage_24h<0 ? 
+                  <p className="coin-percent red ">{item.price_change_percentage_24h.toFixed(2)}%</p>
+                   :
+                  <p className="coin-percent green">{item.price_change_percentage_24h.toFixed(2)}%</p>
+                }
+
+
+
+
+                <p className="coin-marketcap">
+                Market Cap: {item.market_cap}
+                </p>
+              </div>
+            </div>
+          </div>
+         
         ))
       }
     </div>
