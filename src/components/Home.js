c75d0b1fc9fe7d6284bc8f588e2823e441e2fbe3
Burak <mburak.demir.059@gmail.com>
2022-06-08 13:43:24 +0300

    Finished

--- src/components/Home.js
+++ src/components/Home.js
@@ -26,7 +26,9 @@ function Home() {
   const filteredcoins=data.filter(coin=>
     coin.name.toLowerCase().includes(form.name.toLowerCase()))
     
-  return (
+  
+
+    return (
     <div className='home'>
       <h1>Crypto App</h1>
       <form >
